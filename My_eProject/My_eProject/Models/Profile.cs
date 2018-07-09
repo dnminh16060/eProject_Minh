0778edd83c00f9aa32ef40853d9b28bbd6781899
kisstun78 <40550512+kisstun78@users.noreply.github.com>
2018-07-09 13:19:53 +0700

    add profile annotaion

--- My_eProject/My_eProject/Models/Profile.cs
+++ My_eProject/My_eProject/Models/Profile.cs
@@ -22,29 +22,51 @@ namespace My_eProject.Models
         }
     
         public long ID { get; set; }
+
         [Required]
         [Display(Name ="UserName")]
-        [MaxLength(16, ErrorMessage = "User name must be less than 16 characters")]
+        [MaxLength(16, ErrorMessage = "UserName must be less than 16 characters")]
+        [MinLength(6, ErrorMessage = "UserName must be at least 6 characters long")]
         public string UserID { get; set; }
+
         [Required]
+        [DataType(DataType.Password)]
+        [MaxLength(32, ErrorMessage = "Password must be less than 32 characters")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
         public string Password { get; set; }
+
         [Required]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Enter only alphabets")]
+        [MaxLength(20, ErrorMessage = "FirstName must be less than 20 characters")]
         public string FirstName { get; set; }
+
         [Required]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Enter only alphabets")]
+        [MaxLength(20, ErrorMessage = "LastName must be less than 20 characters")]
         public string LastName { get; set; }
+
         [Required]
+        [MaxLength(100, ErrorMessage = "LastName must be less than 100 characters")]
         public string Address { get; set; }
+
         [Required]
+        [RegularExpression("^(01[2689]|09)[0-9]{8}$", ErrorMessage = "Invalid PhoneNumber")]
         public string PhoneNumber { get; set; }
+
         [Required]
         [EmailAddress(ErrorMessage = "Invalid EmailAddress")]
         public string EmailAddress { get; set; }
+
         [Required]
         public bool Sex { get; set; }
+
         [Required]
         public int Age { get; set; }
+
         [Required]
+        [MaxLength(16, ErrorMessage = "LastName must be less than 16 characters")]
         public string CreditCard { get; set; }
+
         public int SkyMiles { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
